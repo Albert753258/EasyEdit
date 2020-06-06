33fb9c5cbfe81539ffa74c7c7af975cdd3790bde
Albert <albert.nasyrov2016@gmail.com>
2020-06-07 00:40:27 +0300

    first commit

---
+++ app/src/main/java/ru/albert/easyedit/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.albert.easyedit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static EditText text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.editText);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int permissionCheck = ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuSave = menu.add(0, 1, 0, "Сохранить");
+        //MenuItem menuOpen = menu.add(0, 1, 0, "Открыть");
+        menuSave.setIntent(new Intent(this, SaveActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/ru/albert/easyedit/SaveActivity.java
@@ -0,0 +1,55 @@
+package ru.albert.easyedit;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+public class SaveActivity extends AppCompatActivity {
+    EditText location;
+    Button saveButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.savefile);
+        location = findViewById(R.id.editText2);
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String disk[] = String.valueOf(location.getText()).split("/");
+                try {
+                    if((!(disk[1].equals("storage")) && !(disk[1].equals("emulated")) && !(disk[1].equals("0"))) || !(disk[1].equals("sdcard"))){
+                        try{
+                            Process process = Runtime.getRuntime().exec(new String[]{"su"});
+                            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+                            os.writeBytes("mount -o rw,remount system /system\n");
+                            String cmd = "echo " + MainActivity.text.getText() + " > " + location.getText() + '\n';
+                            os.writeBytes(cmd);
+                            os.writeBytes("exit\n");
+                            os.flush();
+                        }
+                        catch (IOException e){
+                            Toast.makeText(SaveActivity.this, "No root! Choose another directory", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(location.getText())));
+                        writer.write(String.valueOf(MainActivity.text.getText()));
+                        writer.close();
+                    }
+                }
+                catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+}
