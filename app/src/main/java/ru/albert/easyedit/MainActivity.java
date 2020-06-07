c66b4e7a8b33bb02e7a2972ddd7b834d9a3ac3fb
Albert <albert.nasyrov2016@gmail.com>
2020-06-07 03:00:30 +0300

    Считывание из файлового менеджера

--- app/src/main/java/ru/albert/easyedit/MainActivity.java
+++ app/src/main/java/ru/albert/easyedit/MainActivity.java
@@ -7,12 +7,20 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 
 public class MainActivity extends AppCompatActivity {
     public static EditText text;
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
         }
+        handleIntent();
     }
 
     @Override
@@ -37,5 +46,43 @@ public class MainActivity extends AppCompatActivity {
         //MenuItem menuOpen = menu.add(0, 1, 0, "Открыть");
         menuSave.setIntent(new Intent(this, SaveActivity.class));
         return super.onCreateOptionsMenu(menu);
+    }
+    private void handleIntent() {
+
+        Uri uri = getIntent().getData();
+        if (uri == null) {
+            tellUserThatCouldntOpenFile();
+            return;
+        }
+
+        String tmp = null;
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            tmp = getStringFromInputStream(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (tmp == null) {
+            tellUserThatCouldntOpenFile();
+            return;
+        }
+
+        text.setText(tmp);
+    }
+
+    private void tellUserThatCouldntOpenFile() {
+        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+    }
+
+    public static String getStringFromInputStream(InputStream stream) throws IOException {
+        int n = 0;
+        char[] buffer = new char[1024 * 4];
+        InputStreamReader reader = new InputStreamReader(stream, "UTF8");
+        StringWriter writer = new StringWriter();
+        while (-1 != (n = reader.read(buffer))) writer.write(buffer, 0, n);
+        return writer.toString();
     }
 }
